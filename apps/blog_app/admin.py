293b3825f74d87e95565955559c79ff8555089e6
EspiFreelancer <espifreelancer@gmail.com>
2020-10-21 23:28:06 -0300

    Add blog management in the admin

--- apps/blog_app/admin.py
+++ apps/blog_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, Category
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Post
+    '''
+    readonly_fields = ('created', 'updated')
+
+class CategoryAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Category
+    '''
+    readonly_fields = ('created', 'updated')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+    
--- apps/blog_app/models.py
+++ apps/blog_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=50)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = "categoria"
+		verbose_name_plural = "categorias"
+
+class Post(models.Model):
+	title = models.CharField(max_length=50)
+	content = models.CharField(max_length=50)
+	image = models.ImageField(upload_to='blog', null=True, blank=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	category = models.ManyToManyField(Category)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
+
+	class Meta:
+		verbose_name = "artículo"
+		verbose_name_plural = "artículos"		
