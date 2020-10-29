2771d74696d43e378c2bce172ac9c227e88cd1f0
EspiFreelancer <espifreelancer@gmail.com>
2020-10-28 22:51:51 -0300

    Modify blog_app models and templates

--- apps/blog_app/apps.py
+++ apps/blog_app/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class BlogAppConfig(AppConfig):
     name = 'apps.blog_app'
+    verbose_name = 'Gestor de publicaciones'
--- apps/blog_app/models.py
+++ apps/blog_app/models.py
@@ -1,32 +1,36 @@
 from django.db import models
+from django.utils.timezone import now
 from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
-	name = models.CharField(max_length=50)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50, verbose_name="Nombre")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de edición")
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	class Meta:
-		verbose_name = "categoria"
-		verbose_name_plural = "categorias"
+    class Meta:
+        verbose_name = "categoría"
+        verbose_name_plural = "categorías"
 
 class Post(models.Model):
-	title = models.CharField(max_length=50)
-	content = models.CharField(max_length=50)
-	image = models.ImageField(upload_to='blog', null=True, blank=True)
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
-	categories = models.ManyToManyField(Category, verbose_name="Categorías", related_name="get_posts")
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=50, verbose_name="Título")
+    subtitle = models.CharField(max_length=200, verbose_name="Subtítulo")
+    content = models.TextField(verbose_name="Contenido")
+    image = models.ImageField(upload_to='blog', null=True, blank=True, verbose_name="Imagen")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, verbose_name="Categorías", related_name="get_posts")
+    published = models.DateTimeField(default=now, verbose_name="Fecha de publicación")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de edición")
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
-	class Meta:
-		verbose_name = "artículo"
-		verbose_name_plural = "artículos"		
+    class Meta:
+        verbose_name = "artículo"
+        verbose_name_plural = "artículos"
+        ordering = ['-created']
