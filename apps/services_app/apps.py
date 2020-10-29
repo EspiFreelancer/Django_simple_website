3bd83935bcae94aca3c9daf371237c64b8867015
EspiFreelancer <espifreelancer@gmail.com>
2020-10-28 23:13:51 -0300

    Modify services_app models

--- apps/services_app/apps.py
+++ apps/services_app/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ServicesAppConfig(AppConfig):
     name = 'apps.services_app'
+    verbose_name = 'Gestor de servicios'
--- apps/services_app/models.py
+++ apps/services_app/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Services(models.Model):
-	title = models.CharField(max_length=50)
-	content = models.CharField(max_length=50)
-	image = models.ImageField(upload_to='services')
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now_add=True)
+	title = models.CharField(max_length=50, verbose_name="Título")
+	content = models.CharField(max_length=150, verbose_name="Descripción")
+	image = models.ImageField(upload_to='services', verbose_name="Imagen")
+	created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+	updated = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de edición")
 
 	def __str__(self):
 		return self.title
@@ -15,3 +15,4 @@ class Services(models.Model):
 	class Meta:
 		verbose_name = "servicio"
 		verbose_name_plural = "servicios"
+		ordering = ['-created']
