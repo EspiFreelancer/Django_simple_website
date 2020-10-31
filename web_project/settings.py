b24a4d6f953d0524c6d1e50d22a0a79403705210
EspiFreelancer <espifreelancer@gmail.com>
2020-10-30 21:46:25 -0300

    Create context processors for social networks, modify base.html

--- web_project/settings.py
+++ web_project/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'apps.social_app.processors.context_dict',
             ],
         },
     },
